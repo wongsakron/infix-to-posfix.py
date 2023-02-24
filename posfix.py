0fe6a6f6b8c41f86133923443814d919d2c7df3a
Wongsakron <102412802+wongsakron@users.noreply.github.com>
2023-02-24 00:50:40 -0800

    posfix.py

---
+++ posfix.py
@@ -0,0 +1,51 @@
+import copy
+
+ope = ["+","-","*","/","^","(",")"]
+posfix = []
+stack = []
+data = "(Q+A/j)*B/(C/D+F)*G^D" #datatest
+# data = str(input("Enter Infix"))
+round = True
+
+
+for i in data:
+    if i in ope:
+        stack.append(i)
+        while round == True:
+            if i == ")":
+                del stack[-1]
+                for pushdata in range(1,len(stack)+1):
+                    if stack[-1] != "(":
+                        posfix.append(copy.deepcopy(stack[-1]))
+                        del stack[-1]
+                    else:
+                        del stack[-1]
+                        break
+            if len(stack) > 1:
+                for count in range(0,6):
+                    if stack[-1] in ope[count]:
+                        if count == 1 or count == 3:
+                            top = count -1
+                        else:
+                            top = count 
+                    if stack[-2] in ope[count]:
+                        if count == 1 or count == 3:
+                            after = count -1
+                        else:
+                            after = count 
+                if top > after or after == 5:
+                        break
+                else:
+                    posfix.append(copy.deepcopy(stack[-2]))
+                    del stack[-2]
+            else:
+                break
+    else:
+        posfix.append(i)
+    round = True
+for i in range(1,len(stack)+1):
+    posfix.append(copy.deepcopy(stack[-abs(i)]))
+del stack
+posfix = ' '.join(map(str, posfix))
+print("Infix = " ,data)
+print("Posfix = ", posfix)
